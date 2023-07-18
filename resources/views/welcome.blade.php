3a5282da9a613358d1b20290ad513050f90c32ef
reyl27 <reyfellugo@gmail.com>
2023-07-18 16:44:09 -0400

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-   <link rel="stylesheet" href="../css/indes.css">
+    <link rel="stylesheet" href="../css/indes.css">
     <link rel="stylesheet" href="./fonts/style.css">
     <title>HOME</title>
 </head>
@@ -78,7 +78,7 @@
                 </ul>
 
             </div>
-                      
+
         </section>
 
         <section id="footer-copyright">
