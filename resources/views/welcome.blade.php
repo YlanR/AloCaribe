90ab488ac42a8be102dfdbb6c34faf924d1dc9e0
reyl27 <reyfellugo@gmail.com>
2023-07-19 03:00:15 -0400

    copy3

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,8 +3,9 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/indes.css">
+    <link rel="stylesheet" href="./css/index.css">
     <link rel="stylesheet" href="./fonts/style.css">
+    <link rel="stylesheet" href="./css/plantillas/2.css">
     <title>HOME</title>
 </head>
 <body>
@@ -44,7 +45,31 @@
 
     <main>
         <section></section>
-        <section></section>
+        <!--segunda plantilla -->
+        <section class="plantilla2">
+            <div class="plantilla2-content">
+                <div class="plantilla2-description">
+                    <div class="plantilla2-encapsulado">
+                        <div class="plantilla2-encapsulado-centrado">
+                            <div class="plantilla2-content-espacio"> </div>
+                            <div class="plantilla2-content-top">
+                                <h2>Competencia Nacional</h2>
+                            </div>
+                            <div class="plantilla2-content-mid">
+                                <h1>Salsa Casino</h1>
+                                <h1>BACHATA</h1>
+                            </div>
+                            <div class="plantilla2-content-bot">
+                                <p>14 de Octubre</p>
+                                <p>2023</p>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
         <section></section>
     </main>
 
