f2fcae60f7eeb5e3844185a0cea464f4db039ca9
reyl27 <reyfellugo@gmail.com>
2023-08-05 22:34:29 -0400

    .

---
+++ public/js/agregarCategoria.js
@@ -0,0 +1,10 @@
+
+        let seleccionador = document.getElementById('seleccionadar');
+        let seleccionadorCategorias = document.getElementById('seleccionadarCategorias');
+
+        seleccionador.addEventListener('clikc', mostrar);
+var cate =document.createElement("input");
+function mostrar
+(
+        document.getElementsByTagName("")
+)
