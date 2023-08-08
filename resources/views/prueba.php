b034ce455c3fc4126e30ebc51ab9d5f668ed192d
reyl27 <reyfellugo@gmail.com>
2023-08-08 19:55:38 -0400

    copy 16

--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -2,7 +2,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>lista de competidores</title>
     <style>
 
     </style>
@@ -12,12 +12,7 @@
         <section class="main">
             <div class="main-content">
                 <div class="main-content-contenido">
-                    <h3>Lista de competidores </h3>
-                    <div>
-                        <div></div>
-                        <div></div>
-                        <div></div>
-                    </div>
+                    
                 </div>
             </div>
         </section>
@@ -28,3 +23,4 @@
 </body>
 
 </html>
+
---
+++ resources/views/usuario/listaCompetidores.blade.php
@@ -0,0 +1,91 @@
+<html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>lista de competidores</title>
+    li<link rel="stylesheet" href="../../css/listaCompe.css">
+    </head>
+    <body>
+    <body>
+    <main>
+        <section class="main">
+            <div class="main-content">
+                <div class="main-content-contenido">
+                    <h2>Lista de competidores </h2>
+                    <div class="main-content-contenido-lista lista-competidores">
+
+                        <div class="lista-competidores-genero lista ">
+                            <label for="">Género</label>
+                            <select name="" id="">
+                                <option selected disabled>Select</option>
+                                <option value="">Salsa Casino</option>
+                                <option value="">Bachata</option>
+                            </select>
+                        </div>
+                        <div class="lista-competidores-categoria1 lista ">
+                            <label for="">Categoria</label>
+                            <select name="" id="">
+                                <option selected disabled>Select</option>
+                                <option value="1">Solista Femenido</option>
+                                <option value="2">Solista Masculino</option>
+                                <option value="3">Parejas</option>
+                                <option value="4">Grupos</option>
+                                <option value="5">Duo Masculino</option>
+                                <option value="6">Duo Femenido</option>
+                                <option value="7">Rueda</option>
+                                <option value="8">Coreográfico</option>
+                                <option value="9">Parejas</option>
+                                <option value="10">Lady Style</option>
+                                <option value="11">Men Style</option>
+                            </select>
+                        </div>
+                        <div class="lista-competidores-categoria2 lista">
+                            <label for="">Categoria</label>
+                            <select name="" id="">
+                                <option selected disabled>Select</option>
+                                <option value="">Begginer</option>
+                                <option value="">Nuevo Talento</option>
+                                <option value="">Novel</option>
+                                <option value="">Libre</option>
+                                <option value="">Abierta</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class=" lista-seleccionada">
+                        <table class="lista-seleccionada-tabla">
+                        <thead class="lista-cabecera">
+                                <tr>
+                                    <th>ID</td>
+                                    <th>Academia</td>
+                                    <th>Nombre</td>
+                                    <th>Apellido</td>
+                                    <th>Cédula</td>
+                                    <th>Nivel</td>
+                                    <th>Foto</td>
+                                    <th>Logo</td>
+                                </tr>
+                            </thead>
+        <!--llemar las celdas desde la dba :3 -->
+                            <tbody class="lista-cuerpo">
+                                <tr>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+
+
+    </body>
+</html>
