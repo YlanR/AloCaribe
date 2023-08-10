08a57d952f8aa03220d4065c55e08a168bf18593
reyl27 <reyfellugo@gmail.com>
2023-08-10 13:07:03 -0400

    copy16 sin terminar

--- resources/views/prueba.php
+++ resources/views/prueba.php
@@ -4,6 +4,31 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>lista de competidores</title>
     <style>
+        .plantilla3{
+            display: flex;
+            height: 100%;
+            width:100%;
+        }
+        .plantilla3-izq{
+            display: flex;
+            width: 100%;
+            height: 100%;
+            justify-content: center;
+            align-items: center;
+        }
+        .plantilla3-izq img{
+            height:50%;
+        }
+        .plantilla3-derch img{
+            height:100%;
+        }
+        .imagen-superior{
+            translate: 30px 10px;
+        }
+        .imagen-inferior{
+            translate: -130px -50px;
+            z-index: -1;
+        }
 
     </style>
 </head>
@@ -11,7 +36,16 @@
     <main>
         <section class="main">
             <div class="main-content">
-                <div class="main-content-contenido">
+                <div class="main-content-contenido plantilla3">
+                    <div class="plantilla3-izq">
+                        <div class="plantilla3-adornos"></div>
+                        <img src="./3/imagen2.png" alt="" class="imagen-superior">
+                        <img src="./3/image1.png" alt="" class="imagen-inferior">
+                    </div>
+                    <div class="plantilla3-derch">
+                        <div class="plantilla3-adornos"></div>
+                        <img src="./3/Group4.png" alt="">
+                    </div>
                     
                 </div>
             </div>
